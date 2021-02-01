6d511bf7a85f6873fcdb25519231da69e45c718b
ibrahem mustafa <ib.aldomyate@gmail.com>
2021-02-01 14:18:29 +0200

    v5

--- session 2/app.js
+++ session 2/app.js
@@ -1,16 +1,259 @@
+
+/////////////////////////// STRING ////////////////////////
+// includes => 4
+// trim => 3
+// replace => 2
+// substr => 2
+// repeat => 2
+///////////////////////////////////////////////////////////
+
+/////////////////////////// ARRAY ////////////////////////
+
+// fill => all
+// includes => 6
+// every => 6
+// reduce => 6
+// map => 4
+// concat => 2
+// splice vs slice
+// new Array(10)
+
+///////////////////////////////////////////////////////////
+
 // let
 // const
-
 // arrow function
 // for of
 // for in
-
 // JavaScript Classes
 // JavaScript Promises
 // JavaScript Symbol
+// string
+// number
+// boolean
+// null
+// object
+// Symbol
 // Function Rest Parameter
 // Array.find()
 // Array.findIndex()
+
+///////////////////////////////////////////////////////////////////////////
+/////////////////////////// FIND & FIND INDEX /////////////////////////////
+///////////////////////////////////////////////////////////////////////////
+
+const users = [
+  {
+    id: 1,
+    name: 'ahmed', 
+    age: 15
+  },
+  {
+    id: 2,
+    name: 'mohammed', 
+    age: 25
+  },
+  {
+    id: 3,
+    name: 'ziad', 
+    age: 26
+  },
+  {
+    id: 4,
+    name: 'magdy', 
+    age: 30
+  },
+  
+]
+
+
+const user = users.find(function (user) {
+  return user.id === 3
+})
+
+console.log(user)
+
+const userIndex = users.findIndex(function (user) {
+  return user.id === 3
+})
+
+console.log(userIndex)
+
+
+///////////////////////////////////////////////////////////////////////////
+//////////////////////// Function Rest Parameter //////////////////////////
+///////////////////////////////////////////////////////////////////////////
+
+// function withRestParameter(numberOne, numberTwo, ...arg) {
+//   console.log(arg)
+// }
+
+
+// withRestParameter(1, 2, 3, 4, 5)
+
+
+// function call(index, ...numbers) {
+//   if (index === 'all') {
+//     numbers.forEach(number => console.log('Call Number: ' + number))
+//   } else if (index >= 0) {
+//     console.log('Call Number: ' + numbers[index])
+//   }
+// }
+
+
+// call('all', '0101894561', '0564651310', '015565123132');
+
+
+///////////////////////////////////////////////////////////////////////////
+//////////////////////////////// Symbol //////////////////////////////////
+///////////////////////////////////////////////////////////////////////////
+
+// const user1 = {
+//   id: Symbol('1'),
+//   name: 'ahmed'
+// }
+
+// const user2 = {
+//   id: Symbol('1'),
+//   name: 'ibrahem'
+// }
+
+// const idArr = [ user2.id, user1.id]
+
+// idArr.forEach(id => {
+//   if (user1.id === id) {
+//     console.log(user1)
+//   } else {
+//     console.log(user2)
+//   }
+// })
+
+
+// console.log(user1.id == user2.id)
+
+
+
+///////////////////////////////////////////////////////////////////////////
+//////////////////////// SetTimeout & SetInterval /////////////////////////
+///////////////////////////////////////////////////////////////////////////
+
+// setTimeout
+//  setInterval
+
+// setTimeout(function () {
+//   console.log('One Second Passed')
+// }, 1000);
+
+// let seconds = 0;
+// const interval = setInterval(() => {
+//   seconds++
+//   console.log(seconds + ' seconds Passed')
+
+//   if (seconds === 10) {
+//     clearInterval(interval)
+//   }
+// }, 1000);
+
+
+///////////////////////////////////////////////////////////////////////////
+//////////////////////////////// promise //////////////////////////////////
+///////////////////////////////////////////////////////////////////////////
+
+// New Promise takes a function with two arguments called [ resolve & reject ] || function (resolve, reject) {//your code} || (resolve, reject) => {//your code}
+// {resolve} is standard of completing the process successfully
+// {reject} is standard of failing in proceed the process
+// each one of the [resolve , reject] is a function we can call at attach to it the result
+// in case of resolve we pass the data we want to use in the nest step || resolve(data) || EX: resolve({status: 'success', code: 200})
+// in case of reject we pass the error that represent what's gone wrong || reject(error) || Ex:  reject({status 'failed', code: 404})
+// 
+// promise body is to process some logic and wait until it finished to proceed the rest
+/** 
+ * new Promise((resolve, reject) => {
+ *  do some login 
+ * 
+ * when logic is done and the result is what we need call the resolve function || resolve(data)
+ * else we call the reject || reject(error)
+ * 
+ * })
+ * **/
+
+
+/**
+ * new Promise((resolve, reject) => {
+ *  
+ * 
+ * some code
+ * process
+ * process
+ * process
+ * process
+ * process
+ * process
+ * process
+ * finish
+ * [result]
+ * 
+ * result is Ok ? resolve(result) : reject(error)
+ * 
+ * })
+ *  */  
+
+//  const isOk = new Promise(function (resolve, reject) {
+//    const loadingSuccess = false;
+//    setTimeout(function () {
+//      if (loadingSuccess) {
+//        resolve({
+//          status: 'success',
+//          loaded: true,
+//        })
+//      } else {
+//        reject('Can not load')
+//      }
+//    }, 1000);
+//  })
+
+
+//  isOk.then(function (result) {
+//    console.log(result)
+//  }).catch(function (error) {
+//    console.log(error)
+//  })
+
+// const isOk = new Promise((resolve, reject) => {
+//   const statusOk = true
+//   setTimeout(() => {
+//     statusOk ? resolve({
+//       status: 'success',
+//       isSuccess: true
+//     }) : reject('status Is Not Ok')
+//   }, 1000);
+// })
+
+// isOk.then(result => console.log(result)).catch(err => console.log(err))
+
+
+// fetch('https://jsonplaceholder.typicode.com/todos/1')
+//   .then(response => response.json())
+//   .then(json => console.log(json))
+
+
+// function fetchClone(url) {
+//   return new Promise((resolve, reject) => {
+//     const loaded = false;
+//     let data;
+//     // get data from url;
+//     setTimeout(() => {
+//       if (loaded) {
+//         data = [1,2, 3]
+//         resolve(data)
+//       }else {
+//         reject('Can Not Load Numbers')
+//       }
+//     }, 1000);
+//   })
+// }
+
+// fetchClone('https://getSomeData').then(result => console.log(result)).catch(err => console.log(err))
 
 
 
@@ -73,88 +316,88 @@
 // const bmw = new Car('BMW', 2022, 10000000000000);
 
 
-const usersData = [
-  {
-    id: 1,
-    name: 'ibrahem',
-    age: 30,
-    role: 'admin',
-  },
-  {
-    id: 2,
-    name: 'ahmed',
-    age: 31,
-    role: 'teacher'
-  },
-  {
-    id: 3,
-    name: 'mario',
-    age: 23,
-    role: 'student'
-  },
-  {
-    id: 4,
-    name: 'mohammed',
-    age: 35,
-    role: 'student'
-  },
-  {
-    id: 5,
-    name: 'salah',
-    age: 39,
-    role: 'student'
-  },
-]
+// const usersData = [
+//   {
+//     id: 1,
+//     name: 'ibrahem',
+//     age: 30,
+//     role: 'admin',
+//   },
+//   {
+//     id: 2,
+//     name: 'ahmed',
+//     age: 31,
+//     role: 'teacher'
+//   },
+//   {
+//     id: 3,
+//     name: 'mario',
+//     age: 23,
+//     role: 'student'
+//   },
+//   {
+//     id: 4,
+//     name: 'mohammed',
+//     age: 35,
+//     role: 'student'
+//   },
+//   {
+//     id: 5,
+//     name: 'salah',
+//     age: 39,
+//     role: 'student'
+//   },
+// ]
 
 
-class User {
-  id;
-  name;
-  age;
-  role;
-  balance;
+// class User {
+//   id;
+//   name;
+//   age;
+//   role;
+//   balance;
 
-  constructor(id, name, age, role) {
-    this.id = id;
-    this.name = name;
-    this.age = age;
-    this.role = role;
-    this.balance = 10000;
-  }
-
-
-  isOld() {
-    return this.age > 38
-  }
-
-  inc(amount) {
-    this.balance += amount;
-  } 
+//   constructor(id, name, age, role) {
+//     this.id = id;
+//     this.name = name;
+//     this.age = age;
+//     this.role = role;
+//     this.balance = 10000;
+//   }
 
 
-  print() {
-    console.log('User With Id: ' + this.id + ' -Name: ' + this.name + ' -Age: ' + this.age + ' -WithRole: '+ this.role)
-  }
-}
+//   isOld() {
+//     return this.age > 38
+//   }
 
-const users = [
-];
-
-for (userData of usersData) {
-  const newUserClass = new User(userData.id, userData.name, userData.age, userData.role)
-  users.push(newUserClass)
-}
-
-console.log(users)
-
-const admin = users.find(user => user.role === 'admin');
+//   inc(amount) {
+//     this.balance += amount;
+//   } 
 
 
-users.forEach(user => {
-  user.inc(500);
-  console.log(user.balance)
-  user.print()
-})
+//   print() {
+//     console.log('User With Id: ' + this.id + ' -Name: ' + this.name + ' -Age: ' + this.age + ' -WithRole: '+ this.role)
+//   }
+// }
+
+// const users = [
+// ];
+
+// for (userData of usersData) {
+//   const newUserClass = new User(userData.id, userData.name, userData.age, userData.role)
+//   users.push(newUserClass)
+// }
+
+// console.log(users)
+
+// const admin = users.find(user => user.role === 'admin');
+
+
+// users.forEach(user => {
+//   user.inc(500);
+//   console.log(user.balance)
+//   user.print()
+// })
 
 
 
